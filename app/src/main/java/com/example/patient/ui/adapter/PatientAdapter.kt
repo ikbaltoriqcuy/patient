fc405a9225a56905bd762b805a14906feffd3b9b
ikbaltoriq <ikbalok01@gmail.com>
2021-12-16 15:28:59 +0700

    fixed bug

--- app/src/main/java/com/example/patient/ui/adapter/PatientAdapter.kt
+++ app/src/main/java/com/example/patient/ui/adapter/PatientAdapter.kt
@@ -18,10 +18,10 @@ import com.example.patient.util.Utils
 
 class PatientAdapter(val context: Context, val onDelete:(String)->Unit) : BaseExpandableListAdapter() {
 
-    var data: List<PatientItem>? = null
+    var data: List<PatientItem> = listOf()
 
     override fun getGroupCount(): Int {
-        return data?.size ?: 0
+        return data.size
     }
 
     override fun getChildrenCount(groupPosition: Int): Int {
@@ -29,7 +29,7 @@ class PatientAdapter(val context: Context, val onDelete:(String)->Unit) : BaseEx
     }
 
     override fun getGroup(groupPosition: Int): Any {
-        return data!![groupPosition]
+        return data[groupPosition]
     }
 
     override fun getChild(groupPosition: Int, childPosition: Int): Any {
@@ -55,15 +55,15 @@ class PatientAdapter(val context: Context, val onDelete:(String)->Unit) : BaseEx
         convertView: View?,
         parent: ViewGroup?
     ): View {
-        var view = convertView
+        var view: View? = convertView
         if (view == null) {
             val layoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
             view = layoutInflater.inflate(R.layout.group_item, null)
         }
 
-        val lblTitle = convertView?.findViewById<AppCompatTextView>(R.id.lblTitle)
-        val imgExpand = convertView?.findViewById<AppCompatImageView>(R.id.imgExpand)
-        val imgCollapse = convertView?.findViewById<AppCompatImageView>(R.id.imgCollpase)
+        val lblTitle = view?.findViewById<AppCompatTextView>(R.id.lblTitle)
+        val imgExpand = view?.findViewById<AppCompatImageView>(R.id.imgExpand)
+        val imgCollapse = view?.findViewById<AppCompatImageView>(R.id.imgCollpase)
 
         if (isExpanded) {
             imgExpand?.visibility = View.VISIBLE
@@ -90,27 +90,28 @@ class PatientAdapter(val context: Context, val onDelete:(String)->Unit) : BaseEx
             val layoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
             view = layoutInflater.inflate(R.layout.child_item, null)
         }
-        val lblTime = convertView?.findViewById<AppCompatTextView>(R.id.lblTime)
-        val lblAddress = convertView?.findViewById<AppCompatTextView>(R.id.lblAddress)
-        val lblPhoneNumber = convertView?.findViewById<AppCompatTextView>(R.id.lblPhoneNumber)
-        val imgAvatar = convertView?.findViewById<AppCompatImageView>(R.id.imgAvatar)
+        val lblTime = view?.findViewById<AppCompatTextView>(R.id.lblTime)
+        val lblAddress = view?.findViewById<AppCompatTextView>(R.id.lblAddress)
+        val lblPhoneNumber = view?.findViewById<AppCompatTextView>(R.id.lblPhoneNumber)
+        val imgAvatar = view?.findViewById<AppCompatImageView>(R.id.imgAvatar)
 
         lblTime?.text = Utils.convertTime(data?.get(groupPosition)?.createdAt ?: "")
         lblPhoneNumber?.text = data?.get(groupPosition)?.contact
         lblAddress?.text = data?.get(groupPosition)?.address_2
 
-        val btnDelete = convertView?.findViewById<AppCompatImageView>(R.id.btnDelete)
+        val btnDelete = view?.findViewById<AppCompatImageView>(R.id.btnDelete)
         btnDelete?.setOnClickListener {
             onDelete(data?.get(groupPosition)?.id.toString())
         }
 
-
-        Glide.with(context)
-            .load(Uri.parse(data?.get(groupPosition)?.avatar))
-            .apply(RequestOptions().diskCacheStrategy(DiskCacheStrategy.NONE))
-            .apply(RequestOptions().skipMemoryCache(true))
-            .error(R.drawable.ic_baseline_hourglass_empty_24)
-            .into(imgAvatar!!)
+        imgAvatar?.let { image ->
+            Glide.with(context)
+                .load(Uri.parse(data?.get(groupPosition)?.avatar))
+                .apply(RequestOptions().diskCacheStrategy(DiskCacheStrategy.NONE))
+                .apply(RequestOptions().skipMemoryCache(true))
+                .error(R.drawable.ic_baseline_hourglass_empty_24)
+                .into(image)
+        }
 
         return view!!
     }
